23afbd6e22211a0c524035544b501510167873c3
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-04-08 19:07:37 +0200

    Ajout de // prettier-ignore sur app.js pour éviter le formatage de certaines ligne de code

--- backend/app.js
+++ backend/app.js
@@ -27,18 +27,12 @@ module.exports = mongoose
 //Remplace body.parser, permet d'analyser le corps de la requête
 app.use(express.json());
 
+// prettier-ignore
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+     next();
 });
 
 app.use("/images", express.static(path.join(__dirname, "images")));
